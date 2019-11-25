86121d4fd728aa026b3f9ddb8a3f37880686209c
Pere <pere.solaclaver@gmail.com>
2019-11-25 14:39:11 +0000

    Import paper from material UI

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+// COMPONENTS
+import Dashboard from "./Dashboard";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
 }
 
 export default App;
